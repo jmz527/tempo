893afc9933de286df950668e5fa4467c186137e0
James Rutledge <jarutledge2@gmail.com>
2017-10-23 21:16:36 -0400

    clocks testing imperitive vs functional

--- spotify.js
+++ spotify.js
@@ -3,5 +3,7 @@ const cp = require(`child_process`);
 
 let songID = `1npqP0Epwvvc1jzudkkmVB`
 
-// Plays song
-cp.exec(`open -a "Spotify" spotify:track:${songID}:autoplay:true`);
+// setTimeout(() => {
+	// Plays song
+	cp.exec(`open -a "Spotify" spotify:track:${songID}:autoplay:true`);
+// }, 7200000)
