5a84f3e4a7ddf822cc66cd817540e0e981f330ab
James Rutledge <jarutledge2@gmail.com>
2017-11-07 12:37:50 -0500

    clock stuff

--- clock/clock_functional.js
+++ clock/clock_functional.js
@@ -1,26 +1,21 @@
 addEventListener(`load`, (e) => {
-
 	const compose = (...fns) =>
 		(arg) =>
 			fns.reduce(
 				(composed, f) => f(composed),
 				arg
 			)
-
 	const oneSecond = () => 1000
 	const getCurrentTime = () => new Date()
-	// const clear = () => console.clear()
-	// const log = message => console.log(message)
+	const log = message => console.log(message)
 	const clear = () => document.querySelector('#clock').innerHTML = ``
 	const renderTime = message => document.querySelector('#clock').innerHTML = message
-
 	const serializeClockTime = date =>
 		({
 			hours: date.getHours(),
 			minutes: date.getMinutes(),
 			seconds: date.getSeconds()
 		})
-
 	const civilianHours = clockTime =>
 		({
 			...clockTime,
@@ -28,22 +23,18 @@ addEventListener(`load`, (e) => {
 			clockTime.hours - 12 :
 			clockTime.hours
 		})
-
 	const appendAMPM = clockTime =>
 		({
 			...clockTime,
 			ampm: (clockTime.hours >= 12) ? `PM` : `AM`
 		})
-
 	const display = target => time => target(time)
-
 	const formatClock = format =>
 		time =>
 			format.replace(`hh`, time.hours)
 				  .replace(`mm`, time.minutes)
 				  .replace(`ss`, time.seconds)
 				  .replace(`tt`, time.ampm)
-
 	const prependZero = key => clockTime =>
 		({
 			...clockTime,
@@ -51,35 +42,31 @@ addEventListener(`load`, (e) => {
 			`0` + clockTime[key] :
 			clockTime[key]
 		})
-
 	const convertToCivilianTime = clockTime =>
 		compose(
 			appendAMPM,
 			civilianHours
 		)(clockTime)
-
 	const doubleDigits = civilianTime =>
 		compose(
 			prependZero(`hours`),
 			prependZero(`minutes`),
 			prependZero(`seconds`)
 		)(civilianTime)
-
 	const startTicking = () =>
 		setInterval(
 			compose(
+				// console.clear,
 				clear,
 				getCurrentTime,
 				serializeClockTime,
 				convertToCivilianTime,
 				doubleDigits,
 				formatClock(`hh:mm:ss tt`),
-				// display(log),
-				display(renderTime)
+				display(renderTime),
+				// display(log)
 			),
 			oneSecond()
 		)
-
 	startTicking()
-
 })
